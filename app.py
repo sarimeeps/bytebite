a46533adcbfbf6e722c2439f0cb159b5127e4879
Carson Bavery <ctrackb31@gmail.com>
2024-10-09 17:00:43 -0400

    Added html templates, basic css, and python functionality.

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# The start of bytebite :D
 @app.route('/')
 def index():
-    return '<h1>Hello World</h1>'
+    return render_template('index.html')
+
+@app.route('/database')
+def database():
+    return render_template('database.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
