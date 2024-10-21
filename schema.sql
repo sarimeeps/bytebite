26f2e1efa539949da2efff124ca990a76c44b1c8
Carson Bavery <ctrackb31@gmail.com>
2024-10-21 17:45:36 -0400

    Added user repo, user table, and faux data into schema for testing.

---
+++ schema.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS users (
+user_id SERIAL PRIMARY KEY,
+email VARCHAR(255) NOT NULL UNIQUE,
+username VARCHAR(255) NOT NULL UNIQUE,
+password VARCHAR(255)
+);
+
+
+-- Test Data Only, Do Not Add to Your Real Database
+
+INSERT INTO users (email, username, password)
+VALUES
+    ('johndoe@example.com', 'johndoe', 'password1'),
+    ('bobross@example.com', 'bobross', 'password2'),
+    ('lebronjames@example.com', 'lebronjames', 'password3'),
+    ('sydsarah@example.com', 'sydsarah', 'password4');
