3dd9edad8f6116458a59ce0e3099e6c419dd9849
Muhammad Waheed <mwaheed@uncc.edu>
2024-10-21 18:20:49 -0400

    meal functions

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template, jsonify, request, redirect
 from dotenv import load_dotenv
 import os
 import requests
+import psycopg
+
+# please make a python file name db_secrets.py 
+# and save database password as DB_PASS
+from db_secrets import DB_PASS
 
 load_dotenv()
 
@@ -27,6 +32,49 @@ def calculator():
 @app.route('/builder')
 def builder():
     return render_template('builder.html')
+
+@app.post('/meal')
+def create_meal():
+    
+    name = request.form['name']
+    ingredients = request.form['ingredients']
+    # CHANGE
+    with psycopg.connect(
+        conninfo=f'postgresql://postgres:{DB_PASS}@localhost:5432/Bytebite'
+    ) as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+            'INSERT INTO meals (name, ingredients, favorite) VALUES (%s, %s, %s)',
+            (name, ingredients, False)
+            )
+    return redirect('/builder')
+
+@app.post('/meal/<int:meal_id>/edit')
+def edit_meal(meal_id):
+    name = request.form['name']
+    ingredients = request.form['ingredients']
+    # CHANGE
+    with psycopg.connect(
+        conninfo=f'postgresql://postgres:{DB_PASS}@localhost:5432/Bytebite'
+    ) as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                'UPDATE meals SET name = %s, ingredients = %s WHERE id = %s',
+                (name, ingredients, meal_id)
+            )
+    return redirect('/builder')
+
+@app.post('/meal/<int:meal_id>/delete')
+def delete_meal(meal_id):
+# CHANGE
+    with psycopg.connect(
+        conninfo=f'postgresql://postgres:{DB_PASS}@localhost:5432/Bytebite'
+    ) as conn:
+        with conn.cursor() as cur:
+            cur.execute('DELETE FROM meals WHERE id = %s', (meal_id,)
+                        )
+    return redirect('/builder')
+
 
 # Big Api Call 
 @app.get('/food-list')
